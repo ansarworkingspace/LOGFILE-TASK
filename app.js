2cfb6697f24f24c8734a7b9d110c45b67f41b579
Muhammed Ansar EN <ansarworkingspace@gmail.com>
2023-12-12 11:44:25 +0530

    some updates

--- app.js
+++ app.js
@@ -1,68 +1,64 @@
 
-const fs = require('fs');
-const readline = require('readline');
+const fs = require('fs').promises;
 
 async function findLoyalUsers(logDirectoryPath) {
   const loyalUsers = new Map();
 
-  // Read all log files in the directory asynchronously
-  const files = await fs.promises.readdir(logDirectoryPath);
+  try {
+    const files = await fs.readdir(logDirectoryPath);
 
-  // Process each log file in parallel
-  await Promise.all(files.map(async (file) => {
-    const filePath = `${logDirectoryPath}/${file}`;
+    await Promise.all(files.map(async (file) => {
+      const filePath = `${logDirectoryPath}/${file}`;
+      const content = await fs.readFile(filePath, 'utf8');
+      const lines = content.split('\n');
 
-    // Read and process each log file using a streaming approach
-    const readStream = fs.createReadStream(filePath, 'utf8');
-    const rl = readline.createInterface({ input: readStream });
+      processLogBatch(lines, loyalUsers);
+    }));
 
-    for await (const line of rl) {
-      processLogFile(line, loyalUsers);
+    // Identify and print loyal users
+    for (const [userId, user] of loyalUsers.entries()) {
+      const visitedDays = user.visitDates.size;
+      const isLoyal = visitedDays >= 2 &&
+        Array.from(user.pageClickCount.values()).filter((clicks) => clicks >= 3).length >= 2;
+
+      if (isLoyal) {
+        console.log(`Loyal user: ${userId}`);
+      }
     }
-  }));
-
-  // Identify and print loyal users 
-  for (const [userId, user] of loyalUsers.entries()) {
-    // Check if the user has visited at least 2 days
-    const visitedDays = user.visitDates.size;
-
-    // Check if the user has clicked at least 3 pages on at least 2 different days
-    const isLoyal = visitedDays >= 2 &&
-      [...user.pageClickCount.values()].filter((clicks) => clicks >= 3).length >= 2;
-
-    if (isLoyal) {
-      console.log(`Loyal user: ${userId}`);
-    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
-function processLogFile(line, loyalUsers) {
-  const [timestamp, time, userId, pageVisited] = line.split(' ');
+function processLogBatch(lines, loyalUsers) {
+  for (const line of lines) {
+    const [timestamp, time, userId, pageVisited] = line.split(' ');
 
-  // Correctly parse the date
-  const dateParts = timestamp.split('-').map(part => parseInt(part, 10));
-  const date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+    // Correctly parse the date
+    const dateParts = timestamp.split('-').map(part => parseInt(part, 10));
+    const date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
 
-  // Update user information in the Map
-  if (!loyalUsers.has(userId)) {
-    loyalUsers.set(userId, { visitCount: 0, pageClickCount: new Map(), visitDates: new Set(), uniquePages: new Set() });
-  }
+    // Update user information in the Map
+    if (!loyalUsers.has(userId)) {
+      loyalUsers.set(userId, { visitCount: 0, pageClickCount: new Map(), visitDates: new Set(), uniquePages: new Set() });
+    }
 
-  const user = loyalUsers.get(userId);
+    const user = loyalUsers.get(userId);
 
-  // Check if the user visited on the same day
-  const currentDate = formatDate(date);
-  if (!user.visitDates.has(currentDate)) {
-    user.visitDates.add(currentDate);
-    user.visitCount++;
-    user.pageClickCount.set(currentDate, 0); // Reset the click count for a new day
-    user.uniquePages.clear(); // Clear the set for a new day
-  }
+    // Check if the user visited on the same day
+    const currentDate = formatDate(date);
+    if (!user.visitDates.has(currentDate)) {
+      user.visitDates.add(currentDate);
+      user.visitCount++;
+      user.pageClickCount.set(currentDate, 0); // Reset the click count for a new day
+      user.uniquePages.clear(); // Clear the set for a new day
+    }
 
-  // Check if the clicked page is unique for the day
-  if (!user.uniquePages.has(pageVisited)) {
-    user.uniquePages.add(pageVisited);
-    user.pageClickCount.set(currentDate, user.pageClickCount.get(currentDate) + 1);
+    // Check if the clicked page is unique for the day
+    if (!user.uniquePages.has(pageVisited)) {
+      user.uniquePages.add(pageVisited);
+      user.pageClickCount.set(currentDate, user.pageClickCount.get(currentDate) + 1);
+    }
   }
 }
 
@@ -73,6 +69,6 @@ function formatDate(date) {
   return `${year}-${month}-${day}`;
 }
 
-// Passing the log folder 
+// Passing the log folder
 findLoyalUsers('log_files');
 
